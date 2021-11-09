09749db9ef3562a59bb58c58d36aed563a73a58e
ctcuff <dev.ctcuff@gmail.com>
2021-11-08 21:13:33 -0500

    Added a dummy table to display all items

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,57 @@
-import React, { useState } from 'react';
-import '../style/App.scss';
+import '../style/app.scss';
+import React, { useEffect, useState } from 'react';
+
+type InventoryItem = {
+  id: string;
+  moveable: boolean;
+  description: string;
+  date: string;
+  name: string;
+  quantity: number;
+};
 
 const App = (): JSX.Element => {
-  const [randomNumber, setRandomNumber] = useState(
-    Math.floor(Math.random() * 100)
-  );
+  const [inventoryItems, setInventoryItems] = useState<InventoryItem[]>([]);
 
-  const getRandomNumber = () => {
-    fetch('/csi/random')
+  const getAllItems = async () => {
+    fetch('/csi/api/items/')
       .then(resp => resp.json())
-      .then(resp => setRandomNumber(resp.number))
+      .then(data => setInventoryItems(data))
       .catch(error => {
         // eslint-disable-next-line no-console
         console.warn(error);
       });
   };
 
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Random Number: {randomNumber}</h1>
-      <p>Click the button generate a new random number</p>
-      <button className="generate-btn" onClick={getRandomNumber} type="button">
-        Generate
-      </button>
+      <h1>Inventory</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Movable</th>
+            <th>Name</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {inventoryItems.map(item => (
+            <tr key={item.id}>
+              <td>{item.date}</td>
+              <td>{item.description}</td>
+              <td>{String(item.moveable)}</td>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
